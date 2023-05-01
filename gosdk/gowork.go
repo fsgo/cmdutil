fdd874ace0af7dfa95a387893b4c9c37e99b10c0
hidu <duv123@gmail.com>
2023-05-01 20:53:36 +0800

    auto disable go work

---
+++ gosdk/gowork.go
@@ -0,0 +1,73 @@
+// Copyright(C) 2023 github.com/fsgo  All Rights Reserved.
+// Author: hidu <duv123@gmail.com>
+// Date: 2023/4/11
+
+package gosdk
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"golang.org/x/mod/modfile"
+)
+
+func MustAutoDisableGoWork() {
+	err := AutoDisableGoWork()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TryAutoDisableGoWork() {
+	err := AutoDisableGoWork()
+	if err != nil {
+		log.Println("AutoDisableGoWork failed:", err)
+	}
+}
+
+func AutoDisableGoWork() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, DefaultOrLatest(), "env", "GOWORK")
+	cmd.Stdin = os.Stdin
+	out, err0 := cmd.Output()
+	if err0 != nil {
+		return err0
+	}
+	fp := strings.TrimSpace(string(out))
+	if fp == "off" {
+		return nil
+	}
+	bf, err1 := os.ReadFile(fp)
+	if err1 != nil {
+		return err1
+	}
+	wf, err2 := modfile.ParseWork(fp, bf, nil)
+	if err2 != nil {
+		return err2
+	}
+
+	wd, err3 := os.Getwd()
+	if err3 != nil {
+		return err3
+	}
+	dir := filepath.Dir(fp)
+
+	var found bool
+	for _, u := range wf.Use {
+		ap := filepath.Join(dir, u.Path)
+		if strings.HasPrefix(wd, ap) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		return os.Setenv("GOWORK", "off")
+	}
+	return nil
+}
---
+++ gosdk/gowork_test.go
@@ -0,0 +1,11 @@
+// Copyright(C) 2023 github.com/fsgo  All Rights Reserved.
+// Author: hidu <duv123@gmail.com>
+// Date: 2023/4/11
+
+package gosdk
+
+import "testing"
+
+func TestMustAutoDisableGoWork(t *testing.T) {
+	MustAutoDisableGoWork()
+}
